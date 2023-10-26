576c66b1f926e63220901bb0ede3f133f48f44e5
YashP77 <yash_patel1998@live.com>
2023-10-26 14:12:37 +0100

    003: extra tests added to improve coverage

--- features/login.feature
+++ features/login.feature
@@ -1,3 +1,4 @@
+@FunctionalTest
 Feature: Test Login functionality
   As a user
   I want to be able to log in to my Hudl account
@@ -7,7 +8,7 @@ Feature: Test Login functionality
     Given I am on the homepage
     And I go to the login page
 
-
+  @SmokeTest @RegressionTest1.1
   Scenario Outline: Successful login with valid credentials
     When I enter valid email <email>
     And I enter valid password <password>
@@ -18,24 +19,52 @@ Feature: Test Login functionality
     | email                   | password     |
     | yash_patel1998@live.com | Password123. |
 
-
+  @RegressionTest1.2
   Scenario Outline: Unsuccessful login with valid email and invalid password
     When I enter valid email <email>
     And I enter invalid password <invalidPassword>
     And I click on the continue button
-    Then I should receive an error message
+    Then I should receive the correct error message
 
   Examples:
     | email                   | invalidPassword      |
     | yash_patel1998@live.com | Password123          |
 
-
+  @RegressionTest1.3
   Scenario Outline: Unsuccessful login with invalid email and valid password
     When I enter invalid email <invalidEmail>
     And I enter valid password <password>
     And I click on the continue button
-    Then I should receive an error message
+    Then I should receive the correct error message
 
   Examples:
     | invalidEmail   | password     |
     | yash_patel1998 | Password123. |
+
+
+  @RegressionTest1.4
+  Scenario: Unsuccessful login with empty email and password fields
+    When I click on the continue button
+    Then I should receive the correct error message
+
+
+  @RegressionTest1.5
+  Scenario: Check Forgot Password link redirect
+    When I click on the Forgot Password link
+    Then I should be redirected to the correct page
+
+  @RegressionTest1.6
+  Scenario: Check Create Account link redirect
+    When I click on the Create Account link
+    Then I should be redirected to the correct page
+
+  @RegressionTest1.7
+  Scenario Outline: Check Login with external account link redirect
+    When I click on the continue with <accountType> link
+    Then I should be redirected to the correct <accountType> login page
+
+    Examples:
+    | accountType |
+    | Facebook    |
+#    | Google      |
+#    | Apple       |
--- features/steps/loginStepDef.py
+++ features/steps/loginStepDef.py
@@ -8,6 +8,7 @@ driver = webdriver.Chrome()
 
 @given('I am on the homepage')
 def step_impl(context):
+    time.sleep(5)
     driver.get("https://www.hudl.com")
 
 
@@ -20,68 +21,96 @@ def step_impl(context):
 
 @when("I enter valid email {email}")
 def step_impl(context, email):
-    """
-    :type context: behave.runner.Context
-    :type email: str
-    """
     driver.find_element(By.ID, "email").send_keys(email)
 
 
 @step("I enter valid password {password}")
 def step_impl(context, password):
-    """
-    :type context: behave.runner.Context
-    :type password: str
-    """
     driver.find_element(By.ID, "password").send_keys(password)
 
 
 @step("I click on the continue button")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
     driver.find_element(By.ID, "logIn").click()
 
 
 @then("I should be redirected to the dashboard")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
     time.sleep(1)
     navBarExists = driver.find_element(By.ID, "ssr-webnav").is_displayed()
 
-    assert navBarExists
+    assert navBarExists, "Dashboard not found"
     driver.quit()
 
 
 @step("I enter invalid password {invalidPassword}")
 def step_impl(context, invalidPassword):
-    """
-    :type context: behave.runner.Context
-    :type invalidPassword: str
-    """
     driver.find_element(By.ID, "password").send_keys(invalidPassword)
 
 
-@then("I should receive an error message")
+@then("I should receive the correct error message")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    time.sleep(2)
+    time.sleep(3)
 
-    errorMsgExists = driver.find_element(By.CSS_SELECTOR, ".error-container").is_displayed()
-    assert errorMsgExists
+    if 'RegressionTest1.4' in context.tags:
+        missingInfoErrorMsgExists = driver.find_element(By.ID, "uniName_947Help").is_displayed()
+        assert missingInfoErrorMsgExists, "Missing information error message not found"
+    else:
+        errorMsgExists = driver.find_element(By.CSS_SELECTOR, ".error-container").is_displayed()
+        assert errorMsgExists, "Error message not found"
 
     driver.quit()
 
 
 @when("I enter invalid email {invalidEmail}")
 def step_impl(context, invalidEmail):
-    """
-    :type context: behave.runner.Context
-    :type invalidEmail: str
-    """
     driver.find_element(By.ID, "email").send_keys(invalidEmail)
+
+
+@when("I click on the forgot password link")
+def step_impl(context):
+    driver.find_element(By.LINK_TEXT, "Forgot Password").click()
+
+
+@then("I should be redirected to the correct page")
+def step_impl(context):
+    time.sleep(5)
+
+    if 'RegressionTest1.5' in context.tags:
+        forgotPasswordPageExists = driver.find_element(By.ID, "email-reset-container").is_displayed()
+        assert forgotPasswordPageExists, "Forgot Password page not found"
+    elif 'RegressionTest1.6' in context.tags:
+        createAccountPageExists = driver.find_element(By.ID, "signup-box").is_displayed()
+        assert createAccountPageExists, "Create Account page not found"
+
+    driver.quit()
+
+
+@when("I click on the Create Account link")
+def step_impl(context):
+    driver.find_element(By.ID, "btn-show-signup").click()
+
+
+@when("I click on the continue with {accountType} link")
+def step_impl(context, accountType):
+    if accountType == "Facebook":
+        driver.find_element(By.ID, "btn-facebook-login").click()
+    elif accountType == "Google":
+        driver.find_element(By.ID, "btn-google-login").click()
+    elif accountType == "Apple":
+        driver.find_element(By.ID, "btn-apple-login").click()
+
+
+@then("I should be redirected to the correct {accountType} login page")
+def step_impl(context, accountType):
+    if accountType == "Facebook":
+        facebook_login_link = str(driver.current_url)
+        assert "facebook.com/login" in facebook_login_link, "Facebook login page not found"
+    elif accountType == "Google":
+        google_login_link = str(driver.current_url)
+        assert "accounts.google.com/" in google_login_link, "Google login page not found"
+    elif accountType == "Apple":
+        apple_login_link = str(driver.current_url)
+        assert "appleid.apple.com/" in apple_login_link, "Apple login page not found"
+
+    driver.quit()
